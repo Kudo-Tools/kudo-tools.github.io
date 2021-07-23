babc157f879bc44e4d53fb02094eee3a12229803
Teeds <teedsthemyth@gmail.com>
2021-07-23 14:36:31 -0700

    testing 3

--- functions/saveDiscord.php
+++ functions/saveDiscord.php
@@ -26,8 +26,8 @@ if ($registration == "success"){
     $query = $con->prepare("update accounts set discord_username = :username where user_id = :userId limit 1");
     // $query->bindParam(":username", $username);
     $query->bindParam([
-        'username' => $username,
-        'userId'=>$user_id
+        ':username' => $username,
+        ':userId'=>$user_id
     ]);
     $result = $query->execute();
 
