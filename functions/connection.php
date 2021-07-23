7b24753262bdfd488c3d493f9fe946175d34be8e
Teeds <teedsthemyth@gmail.com>
2021-07-22 23:09:08 -0700

    testing 3

--- functions/connection.php
+++ functions/connection.php
@@ -5,7 +5,7 @@ function establish_connection() {
     $dbuser = "b2335803e8210c";
     $dbpassword = "2670080a";
     $dbname = "heroku_7cc8cb2939f8da4";
-    $con = new PDO($dbhost, $dbuser, $dbpassword, $dbname);
+    // $con = new PDO($dbhost, $dbuser, $dbpassword, $dbname);
     $con = new PDO("mysql:host=".$dbhost.";dbname=".$dbname."", $dbuser, $dbpassword);
     
     if(!$con) {
