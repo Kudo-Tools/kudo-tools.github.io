4b6315e9f5cb03d97a8b7fd1609525af121e29f6
Teeds <teedsthemyth@gmail.com>
2021-07-27 16:56:35 -0700

    revertion change

--- functions/methods.php
+++ functions/methods.php
@@ -38,12 +38,8 @@ function check_login($con, $fromDashboard, $code) {
     if($fromDashboard) {
         // die;
         if(!empty($code)) {
-            echo "method 1";
-            die;
             header("Location: information?code=$code");
         } else {
-            echo "method 2";
-            die;
             header("Location: information?test=nothing");
         }
         // echo "ex = ";
